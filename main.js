917bdf37bc0e08f94905a3a29e97590938706044
ghaith borhan <ghaith@gmail.com>
2022-10-12 12:54:40 +0300

    filter

---
+++ main.js
@@ -0,0 +1,84 @@
+let upload = document.getElementById("upload");
+
+let saturate = document.getElementById("saturate");
+
+let contrast = document.getElementById("contrast");
+
+let brightness = document.getElementById("brightness");
+
+let sepia = document.getElementById("sepia");
+
+let grayscale = document.getElementById("grayscale");
+
+let blur = document.getElementById("blur");
+
+let hue_rotate = document.getElementById("hue_rotate");
+
+let download = document.getElementById("download");
+
+let img = document.getElementById("img");
+
+let reset = document.querySelector("span");
+
+let imgBox = document.querySelector(".img-Box");
+
+let canvas = document.getElementById("canvas"); 
+let ctx = canvas.getContext('2d');
+
+let filters = document.querySelectorAll("ul li input");
+
+
+function resetValue() {
+  ctx.filter = "none";
+  saturate.value = "100";
+  contrast.value = "100";
+  brightness.value = "100";
+  sepia.value = "0";
+  grayscale.value = "0";
+  blur.value = "0";
+  hue_rotate.value = "0";
+}
+
+window.onload = () => {
+  download.style.display = "none";
+  reset.style.display = "none";
+  imgBox.style.display = "none";
+};
+
+upload.onchange = () => {
+  resetValue();
+  download.style.display = "block";
+  reset.style.display = "block";
+  imgBox.style.display = "block";
+  let file = new FileReader();
+  file.readAsDataURL(upload.files[0]);
+  file.onload = () => {
+    img.src = file.result;
+  };
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    img.style.display = 'none';
+  }
+};
+
+filters.forEach((filter) => {
+  filter.addEventListener("input", function () {
+    ctx.filter = `
+    saturate(${saturate.value}%)
+    contrast(${contrast.value}%)
+    brightness(${brightness.value}%)
+    sepia(${sepia.value}%)
+    grayscale(${grayscale.value})
+    blur(${blur.value}px)
+    hue-rotate(${hue_rotate.value}deg)
+    `;
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  });
+});
+
+
+download.onclick = () => {
+  download.href = canvas.toDataURL();
+}
